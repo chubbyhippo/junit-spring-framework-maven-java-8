d7a53e264a8189250e03707b49420267d8784f56
ch <mkmsacc@outlook.com>
2020-11-22 22:30:26 +0700

    Add PassengerUtil

---
+++ src/test/java/PassengerUtil.java
@@ -0,0 +1,11 @@
+public class PassengerUtil {
+
+	public static Passenger getExpectedPassenger() {
+		Passenger passenger = new Passenger("John Smith");
+
+		Country country = new Country("USA", "US");
+		passenger.setCountry(country);
+
+		return passenger;
+	}
+}
