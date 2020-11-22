e5764f48529e3cf8cb82abe8e0ccd4ac429d4ec1
ch <mkmsacc@outlook.com>
2020-11-22 20:48:47 +0700

    Add Country class

---
+++ src/main/java/Country.java
@@ -0,0 +1,17 @@
+public class Country {
+	private String name;
+	private String codeName;
+
+	public Country(String name, String codeName) {
+		this.name = name;
+		this.codeName = codeName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCodeName() {
+		return codeName;
+	}
+}
