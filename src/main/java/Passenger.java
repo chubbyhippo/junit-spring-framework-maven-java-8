3ee05914cee7571dce20a7f6f3be060d31dcae60
ch <mkmsacc@outlook.com>
2020-11-23 19:37:45 +0700

    Add isRegisterd field

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,6 +1,7 @@
 public class Passenger {
 	private String name;
 	private Country country;
+	private boolean isRegistered;
 
 	public Passenger(String name) {
 		this.name = name;
@@ -18,11 +19,20 @@ public class Passenger {
 		this.country = country;
 	}
 
+	public boolean isRegistered() {
+		return isRegistered;
+	}
+
+	public void setRegistered(boolean isRegistered) {
+		this.isRegistered = isRegistered;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + (isRegistered ? 1231 : 1237);
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
@@ -40,6 +50,8 @@ public class Passenger {
 			if (other.country != null)
 				return false;
 		} else if (!country.equals(other.country))
+			return false;
+		if (isRegistered != other.isRegistered)
 			return false;
 		if (name == null) {
 			if (other.name != null)
